673f7d9e8e7231908bb89ea8e0eeee9d6c07bb33
shahzaib Mughal <shahzaib.mughal2014@gmail.com>
2018-07-04 17:53:12 +0500

    changes made

---
+++ MvcSchoolWebApp/Controllers/TMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSchoolWebApp.Controllers
+{
+    public class TMController : Controller
+    {
+        // GET: TM
+        public ActionResult TimeSheet()
+        {
+            return View();
+        }
+    }
+}
