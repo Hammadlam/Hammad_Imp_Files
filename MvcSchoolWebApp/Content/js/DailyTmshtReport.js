8941d70a392a63dd0025b5d62671a45ab7c4adfa
shahzaib Mughal <shahzaib.mughal2014@gmail.com>
2018-07-26 17:24:39 +0500

    daily timesheet.js

---
+++ MvcSchoolWebApp/Content/js/DailyTmshtReport.js
@@ -0,0 +1,20 @@
+﻿
+$("#Btncreaterpt").click(function (e) {
+    $.ajax({
+        url: encodeURI("../TM/CreateDailyReport"),
+        data: {
+            date: $("#DateTMR").val()
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+        debugger
+            window.location.href = '../TM/launch_Daily_report';
+        },
+        error: function (error) {
+            show_err_alert_js('No Record Found');
+            $("#attendanceViewTMR_div").css("display", "none");
+        }
+    });
+});
+
