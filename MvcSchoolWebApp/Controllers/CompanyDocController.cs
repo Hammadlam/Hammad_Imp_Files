d5e59946830897743e2c62e3edb36c70482fd46a
Moiz Mahmood <moiz.mahmood@supernova.com.pk>
2018-09-12 14:15:26 +0500

    Company Doc

---
+++ MvcSchoolWebApp/Controllers/CompanyDocController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSchoolWebApp.Controllers
+{
+    public class CompanyDocController : Controller
+    {
+        // GET: CompanyDoc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult displaydoc()
+        {
+            return View();
+        }
+    }
+}
