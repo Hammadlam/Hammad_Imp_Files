e1d0f0e37326daf92455263ba7ffc554132495ed
Moiz Mahmood <moiz.mahmood@supernova.com.pk>
2018-10-16 14:17:37 +0500

    Applicant form

---
+++ MvcSchoolWebApp/Controllers/HRController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSchoolWebApp.Controllers
+{
+    public class HRController : Controller
+    {
+        // GET: HR
+        public ActionResult Applicantform()
+        {
+            return View();
+        }
+    }
+}
